4bba926185649bf1ab5a8ea757f7ba7c67d05652
João <compras@12tec.com.br>
2025-08-30 20:25:54 -0300

    d

--- app/api/gerar-certificados/route.ts
+++ app/api/gerar-certificados/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: Request) {
 
   try {
     // 1. Gera uma URL para o HTML já pronto (em public/modelos)
-    const htmlUrl = `https://intranet12tec.vercell.app/modelos/${certificado.nome}FRENTE.html`;
+    const htmlUrl = `https://intranet12tec.vercel.app/modelos/${certificado.nome}FRENTE.html`;
 
     // 2. Gera o PDF com Puppeteer
     const browser = await puppeteer.launch();
