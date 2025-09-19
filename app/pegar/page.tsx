1cc6fa6e8cecf1dbed78af2a7f5bdc66064c5e1b
12 tec <compras@12tec.com.br>
2025-09-19 14:36:41 -0300

    VC

---
+++ app/pegar/page.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+interface SharePointData {
+  site: any;
+  drive: any;
+  error?: string;
+}
+
+export default function SharePointPage() {
+  const [data, setData] = useState<SharePointData | null>(null);
+
+  useEffect(() => {
+    fetch('/api/sharepoint')
+      .then(res => res.json())
+      .then(setData)
+      .catch(err => setData({ site: null, drive: null, error: err.message }));
+  }, []);
+
+  if (!data) return <div>Carregando...</div>;
+  if (data.error) return <div>Erro: {data.error}</div>;
+
+  return (
+    <div>
+      <h1>Informações do SharePoint</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
